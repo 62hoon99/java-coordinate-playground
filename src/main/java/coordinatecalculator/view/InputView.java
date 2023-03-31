78896f4e8280ee3889819888845513adcae9c85a
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-04-01 08:19:30 +0900

    feat: 좌표 입력 기능 구현

---
+++ src/main/java/coordinatecalculator/view/InputView.java
@@ -0,0 +1,13 @@
+package coordinatecalculator.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static String point() {
+        System.out.println("좌표를 입력하세요.");
+        return scanner.nextLine();
+    }
+}
