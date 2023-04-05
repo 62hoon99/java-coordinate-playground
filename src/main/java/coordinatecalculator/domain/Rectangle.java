3c193cfe75042f521756e186b54afb3f8e6bdf9e
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-04-05 22:35:13 +0900

    refactor: rectangle 메서드 분리

--- src/main/java/coordinatecalculator/domain/Rectangle.java
+++ src/main/java/coordinatecalculator/domain/Rectangle.java
@@ -8,35 +8,53 @@ import java.util.stream.Collectors;
 
 public class Rectangle {
     public static double calculateArea(List<Point> points) {
+        validateRectangle(points);
+        return getArea(points);
+    }
 
+    private static double getArea(List<Point> points) {
+        Point firstPoint = points.get(0);
+        List<Point> sameLinePoint = getSameLinePoint(points, firstPoint);
+        return calculateArea(firstPoint, sameLinePoint);
+    }
+
+    private static List<Point> getSameLinePoint(List<Point> points, Point firstPoint) {
+        return points.stream()
+                .filter(point -> point.isOnSameLine(firstPoint))
+                .collect(Collectors.toList());
+    }
+
+    private static double calculateArea(Point firstPoint, List<Point> sameLinePoint) {
+        double area = 1;
+        for (Point point : sameLinePoint) {
+            area *= firstPoint.distanceTo(point);
+        }
+        return area;
+    }
+
+    private static void validateRectangle(List<Point> points) {
+        if (countCoordinatesOnSameLine(points) != 4) {
+            throw new IllegalArgumentException("직사각형이 아닙니다.");
+        }
+    }
+
+    private static int countCoordinatesOnSameLine(List<Point> points) {
         int count = 0;
         for (int i = 0; i < points.size(); i++) {
-            int countOnSameLine = 0;
-
-            for (int k = 0; k < points.size(); k++) {
-                if (i != k && points.get(i).isOnSameLine(points.get(k))) {
-                    countOnSameLine++;
-                }
-            }
-
-            if (countOnSameLine == 2) {
+            if (getCountOnSameLine(points, i) == 2) {
                 count++;
             }
         }
+        return count;
+    }
 
-        if (count != 4) {
-            return -1;
+    private static int getCountOnSameLine(List<Point> points, int i) {
+        int countOnSameLine = 0;
+        for (int k = 0; k < points.size(); k++) {
+            if (i != k && points.get(i).isOnSameLine(points.get(k))) {
+                countOnSameLine++;
+            }
         }
-
-        Point firstPoint = points.get(0);
-        List<Point> sameLinePoint = points.stream()
-                .filter(point -> point.isOnSameLine(firstPoint))
-                .collect(Collectors.toList());
-
-        double distance = 1;
-        for (Point point : sameLinePoint) {
-            distance *= firstPoint.distanceTo(point);
-        }
-        return distance;
+        return countOnSameLine;
     }
 }
