cab4b35f8fb8082d061acc19e7dd183fbbc08945
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-04-10 23:03:03 +0900

    refactor: figure factory에 rectangle 추가

--- src/main/java/coordinate/model/FigureFactory.java
+++ src/main/java/coordinate/model/FigureFactory.java
@@ -3,6 +3,7 @@ package coordinate.model;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 
 public class FigureFactory {
@@ -10,23 +11,27 @@ public class FigureFactory {
     private static final String ERROR_INVALID_FIGURE_CREATION = "입력된 Point 개수가 유효하지 않습니다.";
     private static final String NULL_MSG = "좌표값을 입력해주세요.";
     private static final int LINE_SIZE = 2;
+    private static final int RECTANGLE_SIZE = 4;
     private static final Map<Integer, Function<List<Point>, Figure>> figures = new HashMap<>();
 
     public static Figure create(List<Point> points) {
+        setFigures();
         validateNull(points);
         validateSize(points);
-        setFigures();
         return figures.get(points.size()).apply(points);
     }
 
     private static void validateSize(List<Point> points) {
-        if (points.size() != 2) {
-            throw new IllegalArgumentException(ERROR_INVALID_FIGURE_CREATION);
-        }
+        figures.keySet()
+                .stream()
+                .filter(key -> key.equals(points.size()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_INVALID_FIGURE_CREATION));
     }
 
     private static void setFigures() {
         figures.put(LINE_SIZE, Line::new);
+        figures.put(RECTANGLE_SIZE, Rectangle::new);
     }
 
     private static void validateNull(List<Point> points) {
